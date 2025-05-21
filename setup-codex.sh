551717c644723210ae2c05b2b2454566416ced64
serogaq <36307024+serogaq@users.noreply.github.com>
2025-05-21 19:54:03 +0300

    codex: chrome

--- setup-codex.sh
+++ setup-codex.sh
@@ -6,7 +6,10 @@ BUILD_WITH_ANTIZAPRET=${BUILD_WITH_ANTIZAPRET:-0}
 export DEBIAN_FRONTEND=noninteractive
 
 apt update 
-apt install --assume-yes curl wget unzip procps chromium-browser
+apt install --assume-yes curl wget unzip procps
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install ./google-chrome-stable_current_amd64.deb
 
 if [ "$TARGETARCH" == "arm64" ]; then
   apt install gcc-aarch64-linux-gnu
